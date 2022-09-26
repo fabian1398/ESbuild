9f6c28885aea8392d64bd91f80cc97e0b171dae6
Fabian Gutierrez <gutierrez.snehiderfabian@gmail.com>
2022-09-25 23:56:22 -0500

    creacion de las carpetas public, junto a el index.html; y la carpeta src, su archivo index.tsx y la carpeta components con si debido archivo app.tsx

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import APP from './components/App'
+
+ReactDOM.render(
+    <APP message="Hello World"/>,
+    document.getElementById('app')
+)
